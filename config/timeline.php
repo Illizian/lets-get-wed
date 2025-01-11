e2aed545458b91119b0fe52f5988e31fe06a68d1
Alex Scotton <alex@alexscotton.com>
2025-01-11 17:00:00 +0000

    feat: move timeline to config, and add a ceremony page that includes all timeline items

---
+++ config/timeline.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    [
+        'title' => 'Ceremony Guest Arrival',
+        'datetime' => 'Saturday, 28th June 2025, at 14:30',
+        'description' => "Our ceremony is exclusive to close family and friends, because we'll struggle with it in front of them, let alone 90 odd faces 🙈",
+    ],
+    [
+        'title' => 'Ceremony',
+        'datetime' => 'Saturday, 28th June 2025, at 15:00',
+        'description' => 'Let us tie the knot.',
+    ],
+    [
+        'title' => 'Drinks Reception',
+        'datetime' => 'Saturday, 28th June 2025, at 15:40',
+        'description' => 'Raise a glass to the Husband and Wife',
+    ],
+    [
+        'title' => 'Buffet',
+        'datetime' => 'Saturday, 28th June 2025, at 16:20',
+        'description' => 'Snacks for all.',
+    ],
+    [
+        'title' => 'Evening Guest Arrival',
+        'datetime' => 'Saturday, 28th June 2025, at 17:45',
+        'description' => 'The bar opens, and the unwashed masses arrive! We welcome dear friends (and the others) to celebrate with us. BBQ, Cocktails, DJ, and Redwell Beer 🍻!',
+    ],
+    [
+        'title' => 'BBQ',
+        'datetime' => 'Saturday, 28th June 2025, at 19:00',
+        'description' => 'Beef Burgers, Sausages, and grass-fed burgers for the Vegies',
+    ],
+    [
+        'title' => 'Cake!!!',
+        'datetime' => 'Saturday, 28th June 2025, at 19:45',
+        'description' => "The part, if Mez is honest, she's most excited for... Let's cut the cake!",
+    ],
+    [
+        'title' => 'DJ',
+        'datetime' => 'Saturday, 28th June 2025, at 20:00',
+        'description' => "Let's enjoy some tunes, and apparently people do dances? 🤨",
+    ],
+    [
+        'title' => 'Carriages',
+        'datetime' => 'Saturday, 28th June 2025, at 23:00',
+        'description' => "Those camping will gather around the log fires, cuppa of hot cocoa (or maybe a cheeky espresso martini). Those not whisked away like Cinderella (just don't forget your shoes)",
+    ],
+];
