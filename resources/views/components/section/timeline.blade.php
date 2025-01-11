f21caa868cadf7d495474423aa5c9fd2d044e71f
Alex Scotton <alex@alexscotton.com>
2025-01-11 15:44:11 +0000

    feat: abstract timeline into a var

--- resources/views/components/section/timeline.blade.php
+++ resources/views/components/section/timeline.blade.php
@@ -1,3 +1,56 @@
+@php
+    $items = [
+        [
+            'title' => 'Ceremony Guest Arrival',
+            'datetime' => 'Saturday, 28th June 2025, at 14:30',
+            'description' =>
+                "Our ceremony is exclusive to close family and friends, because we'll struggle with it in front of them, let alone 90 odd faces 🙈",
+        ],
+        [
+            'title' => 'Ceremony',
+            'datetime' => 'Saturday, 28th June 2025, at 15:00',
+            'description' => 'Let us tie the knot.',
+        ],
+        [
+            'title' => 'Drinks Reception',
+            'datetime' => 'Saturday, 28th June 2025, at 15:40',
+            'description' => 'Raise a glass to the Husband and Wife',
+        ],
+        [
+            'title' => 'Buffet',
+            'datetime' => 'Saturday, 28th June 2025, at 16:20',
+            'description' => 'Snacks for all.',
+        ],
+        [
+            'title' => 'Evening Guest Arrival',
+            'datetime' => 'Saturday, 28th June 2025, at 17:45',
+            'description' =>
+                'The bar opens, and the unwashed masses arrive! We welcome dear friends (and the others) to celebrate with us. BBQ, Cocktails, DJ, and Redwell Beer 🍻!',
+        ],
+        [
+            'title' => 'BBQ',
+            'datetime' => 'Saturday, 28th June 2025, at 19:00',
+            'description' => 'Beef Burgers, Sausages, and grass-fed burgers for the Vegies',
+        ],
+        [
+            'title' => 'Cake!!!',
+            'datetime' => 'Saturday, 28th June 2025, at 19:45',
+            'description' => "The part, if Mez is honest, she's most excited for... Let's cut the cake!",
+        ],
+        [
+            'title' => 'DJ',
+            'datetime' => 'Saturday, 28th June 2025, at 20:00',
+            'description' => "Let's enjoy some tunes, and apparently people do dances? 🤨",
+        ],
+        [
+            'title' => 'Carriages',
+            'datetime' => 'Saturday, 28th June 2025, at 23:00',
+            'description' =>
+                "Those camping will gather around the log fires, cuppa of hot cocoa (or maybe a cheeky espresso martini). Those not whisked away like Cinderella (just don't forget your shoes)",
+        ],
+    ];
+@endphp
+
 <div class="space-y-2 lg:space-y-4">
     <h3 class="text-5xl font-serif leading-relaxed lg:text-8xl">
         The Cunning Plan
@@ -8,21 +61,8 @@
     </p>
 
     <ol role="list" class="mt-6 space-y-6">
-        <x-timeline-item title="Ceremony Guest Arrival" datetime="Saturday, 28th June 2025, at 14:30"
-            description="Our ceremony is exclusive to close family and friends, because we'll struggle with it in front of them, let alone 90 odd faces 🙈" />
-        <x-timeline-item title="Ceremony" datetime="Saturday, 28th June 2025, at 15:00" />
-        <x-timeline-item title="Drinks Reception" datetime="Saturday, 28th June 2025, at 15:40"
-            description="Raise a glass to the Husband and Wife" />
-        <x-timeline-item title="Buffet" datetime="Saturday, 28th June 2025, at 16:20" description="" />
-        <x-timeline-item title="Evening Guest Arrival" datetime="Saturday, 28th June 2025, at 17:45"
-            description="The bar opens, and the unwashed masses arrive! We welcome dear friends (and the others) to celebrate with us. BBQ, Cocktails, DJ, and Redwell Beer 🍻!" />
-        <x-timeline-item title="BBQ" datetime="Saturday, 28th June 2025, at 19:00"
-            description="Beef Burgers, Sausages, and grass-fed burgers for the Vegies" />
-        <x-timeline-item title="Cake!!!" datetime="Saturday, 28th June 2025, at 19:45"
-            description="The part, if Mez is honest, she's most excited for... Let's cut the cake!" />
-        <x-timeline-item title="DJ" datetime="Saturday, 28th June 2025, at 20:00"
-            description="Let's enjoy some tunes, and apparently people do dances? 🤨" />
-        <x-timeline-item title="Carriages" datetime="Saturday, 28th June 2025, at 23:00"
-            description="Those camping will gather around the log fires, cuppa of hot cocoa (or maybe a cheeky espresso martini). Those not whisked away like Cinderella (just don't forget your shoes)" />
+        @foreach ($items as $item)
+            <x-timeline-item :title="$item['title']" :datetime="$item['datetime']" :description="$item['description']" />
+        @endforeach
     </ol>
 </div>
