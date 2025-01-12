0ee97037ffeee7375d0c791d1ebd3a46b88f6c9a
Alex Scotton <alex@alexscotton.com>
2025-01-12 12:51:06 +0000

    feat: swap precendence, homepage is ceremony, /reception for non-ceremony

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,15 @@ use App\Models\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $timeline = array_slice(config('timeline'), 4);
-
-    return view('home', compact('timeline'));
-});
-
-Route::get('/ceremony', function () {
     $timeline = config('timeline');
 
     return view('ceremony', compact('timeline'));
+});
+
+Route::get('/reception', function () {
+    $timeline = array_slice(config('timeline'), 4);
+
+    return view('reception', compact('timeline'));
 });
 
 Route::get('/rsvp/{response}', function (Response $response) {
